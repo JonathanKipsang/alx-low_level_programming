48f9a9058d4e81ff45e4ac95caad577c889822f8
JonathanKipsang <jonathankipchirchir7@gmail.com>
2023-03-13 04:14:50 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,13 @@
 */
 char *create_array(unsigned int size, char c)
 {
-char *str
-unasigned int i
-str=malloc(sizeof(char)*size);
-if (size == 0 || str == null)
+char *str;
+unsigned int i;
+str = malloc(sizeof(char) * size);
+if (size == 0 || str == NULL)
 return (NULL);
-for (i=0;i<size;i++)
-str [i]=c;
-return (str)
+for (i = 0; i < size; i++)
+str[i] = c;
+return (str);
 }
+
