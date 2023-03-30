7e4ee31d5e9bfd096308148ad2aa319833ab80df
JonathanKipsang <jonathankipchirchir7@gmail.com>
2023-03-30 01:41:24 -0700

    task 5

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -1,17 +1,15 @@
 #include "0-object_like_macro.h"
 #include "0-object_like_macro.h"
 #include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    int s;
-
-    s = 98 + SIZE;
-    printf("%d\n", s);
-    return (0);
+int s;
+s = 98 + SIZE;
+printf("%d\n", s);
+return (0);
 }
