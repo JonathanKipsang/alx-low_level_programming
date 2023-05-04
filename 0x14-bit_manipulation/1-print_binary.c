c008a1c303d0c622dcc19dce93f7437a583bd6ea
JonathanKipsang <jonathankipchirchir7@gmail.com>
2023-05-04 02:44:21 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_binary - prints a binary whuch is equal to the decimal of no
+* @n: number to be  printed in binary
+*/
+void print_binary(unsigned long int n)
+{
+int i, count = 0;
+unsigned long int current;
+for (i = 63; i >= 0; i--)
+{
+current = n >> i;
+if (current & 1)
+{
+_putchar('1');
+count++;
+}
+else if (count)
+_putchar('0');
+}
+if (!count)
+_putchar('0');
+}
+
